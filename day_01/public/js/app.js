2e3bb89eae4bca93201f54a4e63211dc03a9664e
NaderBhrr <nader.bhr.devv@gmail.com>
2020-10-27 13:58:11 +0330

    Add the function to remove the playing class after transition ends

--- day_01/public/js/app.js
+++ day_01/public/js/app.js
@@ -1,19 +1,22 @@
 // To listen to a "keydown" event
-window.addEventListener("keydown", (event) => {
-  // Chosse the audio element that corresponds with the event.key
+const playSound = (event) => {
+  // Choose the audio element that corresponds with the event.key
   const audio = document.querySelector(`audio[data-key="${event.key}"`);
   const key = document.querySelector(`div[data-key="${event.key}"`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
-});
+};
 
 // To manage the transition style when it is completed
-const removeTransition = event => {
-    console.log(event);
+function removeTransition(event) {
+  if (event.propertyName !== "transform") return;
+  this.classList.remove("playing");
 }
 const keys = document.querySelectorAll(".key");
 for (key of keys) {
   key.addEventListener("transitionend", removeTransition);
 }
+
+window.addEventListener("keydown", playSound);
