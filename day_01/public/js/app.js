4986c0c2ce385e3bbd9bf22bcc48e80a0383872c
NaderBhrr <nader.bhr.devv@gmail.com>
2020-10-27 10:49:03 +0330

    Add key variable for the corresponding key of the selected audio element

--- day_01/public/js/app.js
+++ day_01/public/js/app.js
@@ -2,7 +2,9 @@
 window.addEventListener("keydown", (event) => {
   // Chosse the audio element that corresponds with the event.key
   const audio = document.querySelector(`audio[data-key="${event.key}"`);
-  if (!audio) return
+const key = document.querySelector(`div[data-key="${event.key}"`)
+console.log(key);
+  if (!audio) return;
+  audio.currentTime = 0;
   audio.play();
- 
 });
