1366a18560cf6f32a23c2e14559b516d9a245ad8
NaderBhrr <nader.bhr.devv@gmail.com>
2020-10-27 12:25:41 +0330

    Add functionality to manage the transitionend event for keys

--- day_01/public/js/app.js
+++ day_01/public/js/app.js
@@ -2,10 +2,18 @@
 window.addEventListener("keydown", (event) => {
   // Chosse the audio element that corresponds with the event.key
   const audio = document.querySelector(`audio[data-key="${event.key}"`);
-const key = document.querySelector(`div[data-key="${event.key}"`)
-console.log(key);
+  const key = document.querySelector(`div[data-key="${event.key}"`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-  key.classList.add('playing')
+  key.classList.add("playing");
 });
+
+// To manage the transition style when it is completed
+const removeTransition = event => {
+    console.log(event);
+}
+const keys = document.querySelectorAll(".key");
+for (key of keys) {
+  key.addEventListener("transitionend", removeTransition);
+}
