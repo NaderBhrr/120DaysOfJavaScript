6144e38827c5ba2fe945c46fd5ed3694accd949e
NaderBhrr <nader.bhr.devv@gmail.com>
2020-10-27 10:32:33 +0330

    Create eventListener to match "keydown" event to play audio tag

--- day_01/public/js/app.js
+++ day_01/public/js/app.js
@@ -1,9 +1,6 @@
 // To listen to a "keydown" event
-window.addEventListener('keydown', (event) => {
-console.log("The event: ", event.key);  
-
-// Chosse the audio element that corresponds with the event.key
-const audio = document.querySelector(`audio[data-key="${event.key}"`)
-console.log(audio);
-audio.play();
-})
+window.addEventListener("keydown", (event) => {
+  // Chosse the audio element that corresponds with the event.key
+  const audio = document.querySelector(`audio[data-key="${event.key}"`);
+  audio.play();
+});
